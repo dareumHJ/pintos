2afd94b3ec80dddc4abc168d94be6fbf1e758566
lsjche123 <lsjche123@naver.com>
2024-10-06 15:17:12 +0000

    Added a struct lock for synchronization during read() and write()

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -1,23 +1,26 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 #include <stdio.h>
+#include "threads/synch.h"
+
+struct lock syscall_lock;
 
 void syscall_init (void);
 void check_address (void *addr);
 
-// void halt (void);
+void halt (void);
 void exit (int status);
 // int fork (const char *thread_name);
 // int exec (const char *cmd_line);
 // int wait (int pid);
-// bool create (const char *file, unsigned initial_size);
-// bool remove (const char *file);
-// int open (const char *file);
-// int filesize (int fd);
-// int read (int fd, void *buffer, unsigned size);
-// int write (int fd, const void *buffer, unsigned size);
-// void seek (int fd, unsigned position);
-// unsigned tell (int fd);
-// void close (int fd);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
 
 #endif /* userprog/syscall.h */
